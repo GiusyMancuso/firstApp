910f84bf26cb78c7deaa90703347961a6580a4a4
Giusy Mancuso <giusy219@gmail.com>
2022-04-12 16:48:08 +0200

    added activity and bottom navigation bar

--- app/src/main/java/com/android/example/cartoleria/FirstFragment.kt
+++ app/src/main/java/com/android/example/cartoleria/FirstFragment.kt
@@ -29,6 +29,14 @@ class FirstFragment : Fragment() {
 
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonSecond.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_homeFragment2)
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
